327a44888f18a84a7b5c667b2551d5de4a6dd698
kinuko1221 <kinuko1221@net44-dhcp44.sfc.keio.ac.jp>
2016-04-21 11:53:39 +0900

    id、add1、add2、distanceの定義

---
+++ js/id.js
@@ -0,0 +1,14 @@
+function id(value){
+  return value;
+}
+
+function add1(value){
+    return value + 1;
+}
+
+function add2(value){
+    return value + 2;
+}
+function distance(a, b, c, d){
+    return Math.sqrt((c - a) * (c - a) + (d - b) * (d - b));
+}
